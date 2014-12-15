f35a3115d761667faceb2dfa8ad6a432032dae0f
Rune Madsen <rune@runemadsen.com>
2014-12-15 16:10:42 +0100

    new ipython plugin

--- examples/ipython.js
+++ examples/ipython.js
@@ -3,42 +3,135 @@
     window.atlasplugins = {};
   }
 
+  window.atlasplugins.ipythonServer = "jupyter-kernel.odewahn.com";
+
   atlasplugins.IPythonRunner = (function() {
+    IPythonRunner.prototype.SERVER = void 0;
+
+    IPythonRunner.prototype.TEMPLATE = _.template("<div class=\"ipython\">\n<div class=\"control-area\">\n  <a href=\"#\" class=\"run\">Run</a>\n</div>\n<div class=\"display-area\">\n</div>\n</div>");
+
     function IPythonRunner(el) {
-      var kernel;
-      this.id = "ipython-" + window.ipython++;
-      this.state = this.STATE_CODE;
       this.$el = $(el);
       this.code = this.$el.html();
-      this.template = _.template("<div class=\"ipython\">\n<div class=\"control-area\">\n  <a href=\"#\" class=\"run\">Run</a>\n</div>\n<div class=\"display-area\">\n</div>\n</div>");
-      this.$newEl = $(this.template({
+      this.$newEl = $(this.TEMPLATE({
         code: this.code
       }));
       this.$el.replaceWith(this.$newEl);
-      IPython.tooltip = new IPython.Tooltip();
-      kernel = new IPython.Kernel('/kernels');
-      kernel._kernel_started({
-        kernel_id: '1',
-        ws_url: atlasplugins.ipythonServer
-      });
-      this.cell = new IPython.CodeCell(kernel);
+      this.cell = new IPython.CodeCell(window.atlasplugins.ipkernel);
       this.$newEl.find(".display-area").append(this.cell.element);
       this.cell.set_text(this.code);
-      this.cell.select();
       this.$newEl.find(".control-area .run").click((function(_this) {
         return function(e) {
-          _this.cell.execute();
-          return e.preventDefault();
+          e.preventDefault();
+          return _this.clicked();
         };
       })(this));
     }
+
+    IPythonRunner.prototype.clicked = function(e) {
+      var server;
+      if (this.SERVER != null) {
+        return this.run();
+      } else {
+        server = this.get_server();
+        if (server && server.url) {
+          return this.check_server((function(_this) {
+            return function() {};
+          })(this));
+        } else {
+          return this.create_server((function(_this) {
+            return function() {
+              return _this.run();
+            };
+          })(this));
+        }
+      }
+    };
+
+    IPythonRunner.prototype.check_server = function(cb) {
+      console.log("Checking server connection");
+      return $.ajax({
+        url: "http://" + atlasplugins.ipythonServer + ":8000" + (this.get_server().url) + "/",
+        dataType: "JSON",
+        method: "GET"
+      }).done((function(_this) {
+        return function(data) {
+          console.log("--> Good");
+          _this.save_server(data.url);
+          _this.connect_server();
+          return _this.run(1000);
+        };
+      })(this)).error((function(_this) {
+        return function(e1, e2, e3) {
+          console.log("--> Bad");
+          return _this.create_server(cb);
+        };
+      })(this));
+    };
+
+    IPythonRunner.prototype.create_server = function(cb) {
+      console.log("Creating new server");
+      return $.ajax({
+        url: "http://" + atlasplugins.ipythonServer + ":8000/spawn",
+        dataType: "JSON",
+        method: "GET"
+      }).done((function(_this) {
+        return function(data) {
+          console.log("--> Good");
+          _this.save_server(data.url);
+          _this.connect_server();
+          return _this.run(1000);
+        };
+      })(this)).error(function(e1, e2, e3) {
+        return console.log("--> Failed");
+      });
+    };
+
+    IPythonRunner.prototype.save_server = function(url) {
+      url = url.replace(/\/$/, "");
+      this.SERVER = url;
+      return $.cookie('ipythonserver', {
+        url: url
+      });
+    };
+
+    IPythonRunner.prototype.get_server = function() {
+      return $.cookie('ipythonserver');
+    };
+
+    IPythonRunner.prototype.connect_server = function() {
+      var url;
+      url = "ws://" + atlasplugins.ipythonServer + ":8000" + this.SERVER;
+      return atlasplugins.ipkernel._kernel_started({
+        kernel_id: '1',
+        ws_url: url
+      });
+    };
+
+    IPythonRunner.prototype.run = function(delay) {
+      if (delay == null) {
+        delay = 0;
+      }
+      if (delay > 0) {
+        return setTimeout((function(_this) {
+          return function() {
+            return _this.cell.execute();
+          };
+        })(this), delay);
+      } else {
+        return this.cell.execute();
+      }
+    };
 
     return IPythonRunner;
 
   })();
 
   $(function() {
-    return $('pre[data-executable="ipython"]').each(function(pre) {
+    $.cookie.json = true;
+    IPython.tooltip = new IPython.Tooltip();
+    window.atlasplugins.ipkernel = new IPython.Kernel('/kernels');
+    return $('pre[data-executable]').each(function(pre) {
       return new atlasplugins.IPythonRunner(this);
     });
   });
--- plugins/ipython/javascripts/ipython.coffee
+++ plugins/ipython/javascripts/ipython.coffee
@@ -1,14 +1,10 @@
 window.atlasplugins = {} unless window.atlasplugins
+window.atlasplugins.ipythonServer = "jupyter-kernel.odewahn.com";
 
 class atlasplugins.IPythonRunner
 
-  constructor: (el) ->
-
-    @id = "ipython-" + window.ipython++
-    @state = @STATE_CODE
-    @$el = $(el)
-    @code = @$el.html()
-    @template = _.template("""<div class="ipython">
+  SERVER: undefined
+  TEMPLATE : _.template("""<div class="ipython">
   <div class="control-area">
     <a href="#" class="run">Run</a>
   </div>
@@ -17,28 +13,114 @@ class atlasplugins.IPythonRunner
 </div>
 """)
 
-    @$newEl = $(@template(
-      code: @code
-    ))
+  constructor: (el) ->
+
+    # Render template instead of code example
+    @$el = $(el)
+    @code = @$el.html()
+    @$newEl = $(@TEMPLATE(code: @code))
 
     @$el.replaceWith(@$newEl)
-
-    # This should be tweaked to handle multiple areas?
-    IPython.tooltip = new IPython.Tooltip()
-    kernel = new IPython.Kernel('/kernels')
-    kernel._kernel_started({kernel_id: '1', ws_url: atlasplugins.ipythonServer})
-    @cell = new IPython.CodeCell(kernel)
+    @cell = new IPython.CodeCell(window.atlasplugins.ipkernel)
     
     @$newEl.find(".display-area").append(@cell.element)
     @cell.set_text(@code)
-    @cell.select()
 
-    @$newEl.find(".control-area .run").click((e) =>
-      @cell.execute();
+    # Click event
+    @$newEl.find(".control-area .run").click((e) => 
       e.preventDefault()
+      @clicked()
     )
+
+  clicked: (e) ->
+    # if we already have a live connection
+    if @SERVER?
+      @run()
+    # if we don't have a live connection
+    else
+      # Do we have a server saved in cookie?
+      server = @get_server()
+      if server && server.url
+        # Is it live?
+        @check_server(=>
+          # connect
+          # run (delay?)
+        )  
+      # Create a new server
+      else
+        @create_server(=>
+          @run()
+        )
   
+  # Will check a connection to a server. If good, it calls callback.
+  # If connection is dead, it'll spawn a new server and call callback.
+  check_server: (cb) ->
+    console.log "Checking server connection"
+    $.ajax(
+      url: "http://#{atlasplugins.ipythonServer}:8000#{@get_server().url}/",
+      dataType: "JSON"
+      method: "GET"
+    ).done((data) =>
+      console.log "--> Good"
+      @save_server(data.url)
+      @connect_server()
+      @run(1000)
+    ).error((e1, e2, e3) =>
+      console.log "--> Bad"
+      @create_server(cb)
+    )
+
+  # Creates a new server and calls callback.
+  create_server: (cb) ->
+    console.log "Creating new server"
+    $.ajax(
+      url: "http://#{atlasplugins.ipythonServer}:8000/spawn",
+      dataType: "JSON"
+      method: "GET"
+    ).done((data) =>
+      console.log "--> Good"
+      @save_server(data.url)
+      @connect_server()
+      @run(1000)
+    ).error((e1, e2, e3) ->
+      console.log "--> Failed"
+    )
+
+  # Saves a server in a cookie to preserve across pages
+  save_server: (url) ->
+    url = url.replace(/\/$/, "") # remove trailing slash
+    @SERVER = url
+    $.cookie('ipythonserver', {url:url})
+
+  # Gets server from cookie
+  get_server: ->
+    $.cookie('ipythonserver')
+
+  # Connects to existing kernel
+  connect_server: ->
+    url = "ws://#{atlasplugins.ipythonServer}:8000#{@SERVER}"
+    atlasplugins.ipkernel._kernel_started({kernel_id: '1', ws_url: url})
+
+  # runs the code example
+  run: (delay=0) ->
+    if delay > 0
+      setTimeout(=>
+        @cell.execute()
+      , delay) 
+    else
+      @cell.execute()
+
 $(->
-  $('pre[data-executable="ipython"]').each((pre) -> new atlasplugins.IPythonRunner(this))
+
+  # allow saving JSON in cookies
+  $.cookie.json = true;
+
+  # initialize iPython kernel
+  IPython.tooltip = new IPython.Tooltip()
+  window.atlasplugins.ipkernel = new IPython.Kernel('/kernels')
+
+  # Automatically initialize ipython for specificed code examples.
+  $('pre[data-executable]').each((pre) -> new atlasplugins.IPythonRunner(this))
+
 )
     
